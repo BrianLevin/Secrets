ded9a9fbd986bc0705bd6e44bc7caadecfd5892a
BrianLevin <bml201095@gmail.com>
2021-05-07 11:17:04 -0400

    added mongoose and mongo connection and added shcema and model for users

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser= require("body-parser");
 
 const ejs= require("ejs");
 
+const mongoose = require("mongoose");
+
 const app = express();
 
  app.use(express.static("public"));
@@ -11,6 +13,20 @@ const app = express();
  app.set("view engine", "ejs");
 
  app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+ const userSchema =  {
+email: String,
+password: String
+
+ };
+
+ const User = new mongoose.model("User", userSchema)
+
+
 
 
 
