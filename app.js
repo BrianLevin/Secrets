d1c96d17d9bb78691b9aa76054b5c21680993c1f
BrianLevin <bml201095@gmail.com>
2021-06-15 21:25:43 -0400

    added a port to the sevrer

--- app.js
+++ app.js
@@ -266,7 +266,7 @@ passport.authenticate("local") (req,res, function(){
 
 
 
- app.listen(3000, function(){
+ app.listen(process.env.PORT||3000, function(){
 
     console.log("Server started on port 3000")
  })
